45ffcac1a94b6ac482bb49be5d22818daee6251c
Ryan Nguyen <ryanvannguyen@gmail.com>
2024-07-26 15:01:22 -0400

    removed unnecessary post request

--- app/projects/[slug]/view.tsx
+++ app/projects/[slug]/view.tsx
@@ -4,13 +4,13 @@ import { useEffect } from "react";
 
 export const ReportView: React.FC<{ slug: string }> = ({ slug }) => {
 	useEffect(() => {
-		fetch("/api/incr", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ slug }),
-		});
+		// fetch("/api/incr", {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 	},
+		// 	body: JSON.stringify({ slug }),
+		// });
 	}, [slug]);
 
 	return null;
